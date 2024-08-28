0e6447a3fbfb9d70088a8019da11c4758112a694
Niclas L <niclas.lund@ntig.se>
2024-08-28 14:55:54 +0200

    Code boilerplate done.

---
+++ max_diff.py
@@ -0,0 +1,8 @@
+# Största skillnad
+
+def main():
+    # Skriv din lösning här nedan. Byt ut "pass" mot din kod.
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ mean_value.py
@@ -0,0 +1,8 @@
+# Medelvärde
+
+def main():
+    # Skriv din lösning här nedan. Byt ut "pass" mot din kod.
+    pass
+
+if __name__ == "__main__":
+    main()
